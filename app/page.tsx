aacff3b4ca0cfe48eba09f0408cfb0977f2dc6e4
Anton <antonkoller@outlook.com>
2023-12-12 10:51:29 +0100

    updated main page text

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.introDiv}>
-        Welcome to Season of Discovery Clues
+        Welcome to Season of Discovery: Clues
       </div>
     </main>
   );
