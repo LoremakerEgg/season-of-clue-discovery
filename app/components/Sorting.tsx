d09af1702df8c0fb7dcac147bb4e9c3b98669461
Anton <antonkoller@outlook.com>
2023-12-15 13:37:47 +0100

    added blanket race selection on class page

--- app/components/Sorting.tsx
+++ app/components/Sorting.tsx
@@ -108,6 +108,10 @@ const Sorting = (props: any) => {
         <div className={styles.title}>
           <h1>{props.class} Runes</h1>
         </div>
+        <Races
+          raceOneImage={props.raceOneImage ? props.raceOneImage : ""}
+          raceTwoImage={props.raceTwoImage ? props.raceTwoImage : ""}
+        />
         <div className={styles.displayChoiceDiv}>
           <h2 onClick={zoneSortClick} style={{ cursor: "pointer" }}>
             Sort By Zone
